3d9927611a13f7b74e4790704e09764358b0a209
Patrick <patrickk0806@hotmail.com>
2019-12-24 14:21:57 -0300

    Full contact app

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:agenda_contatos/ui/contact_page.dart';
+import 'package:agenda_contatos/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home:HomePage(),
+));
